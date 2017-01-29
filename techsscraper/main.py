1af88304bcdce28eeeb8c5fe0dd60e74b7498340
Victor Li <victor@victorli.nl>
2017-01-29 19:29:09 +0100

    Add simple main

---
+++ techsscraper/main.py
@@ -0,0 +1,9 @@
+import scraper
+
+
+def main(args=None):
+    scraper.scrape_all_blogs()
+
+
+if __name__ == "__main__":
+    main()
---
+++ techsscraper/scraper.py
@@ -0,0 +1,8 @@
+import yaml
+
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+
+def scrape_all_blogs():
+    pass

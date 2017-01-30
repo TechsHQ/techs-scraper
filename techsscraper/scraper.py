c0ef877f78e3fa3ce6a59ce20a585d42fb8b1c04
Victor Li <victor@victorli.nl>
2017-01-30 23:03:40 +0100

    Add scraper intital implementation

--- techsscraper/scraper.py
+++ techsscraper/scraper.py
@@ -5,4 +5,11 @@ from scrapy.utils.project import get_project_settings
 
 
 def scrape_all_blogs():
-    pass
+    blogs = yaml.load(open('../blogs.yaml', 'r'))
+
+    process = CrawlerProcess(get_project_settings())
+
+    for blog in blogs:
+        process.crawl(blogs[blog]['spider'], url=blogs[blog]['url'])
+
+    process.start()

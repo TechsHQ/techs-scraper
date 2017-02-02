dd13699c6d9341ebf53256387f78ea84a557f80a
Victor Li <victor@victorli.nl>
2017-02-03 00:01:20 +0100

    Fix bug in get_db_name

--- techsscraper/util.py
+++ techsscraper/util.py
@@ -8,7 +8,8 @@ def clean(data):
 def serialize_date(value):
     return parser.parse(value)
 
+
 def get_db_name(uri):
     arr = uri.split('/')
     if len(arr) >= 4:
-        return arr[3]
+        return arr[len(arr) - 1]
